38d5023430b061f51112750896d948ac78014f1b
sueunkim <silversu3208@naver.com>
2023-03-19 13:29:26 +0900

    충돌마지막

--- front-office/ops/src/App.js
+++ front-office/ops/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          안ffftttttdsasdfsdafasdfsadfsadasdf
+          ㅁㅁㅁ
         </a>
       </header>
     </div>
