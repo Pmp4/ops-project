b91ca4d1db40f5d6786bf468bff00ca2adb58ebd
P__.mp4 <postlist.ed@gmail.com>
2023-03-21 23:11:38 +0900

    1차 환경설정

---
+++ back-office/ops/ops-api/src/main/java/com/pmp4/ops/config/DBConfiguration.java
@@ -0,0 +1,29 @@
+package com.pmp4.ops.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+@EnableTransactionManagement
+public class DBConfiguration {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(hikariConfig());
+    }
+}
