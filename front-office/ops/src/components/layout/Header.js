aba2597a23b028331f158eb67f4bf14047b1cf54
P__.mp4 <postlist.ed@gmail.com>
2023-03-25 22:33:55 +0900

    REACT Header navigator 수정

--- front-office/ops/src/components/layout/Header.js
+++ front-office/ops/src/components/layout/Header.js
@@ -1,4 +1,6 @@
-import Logo from "img/logo.svg"
+import { faGear, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Logo from "img/logo.svg";
 import React, { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
@@ -6,33 +8,40 @@ const Header = () => {
     const location = useLocation();
 
     useEffect(() => {
-
+        console.log(location.pathname.split("/"))
+        console.log(location.pathname)
     }, [location]);
 
-
     return (
-        <div id="header" className='box'>
+        <div id="header" className="box">
             <div className="header-wrap flex-wrap">
                 <div className="logo">
                     <Link to="/">
-                        <img src={Logo} alt="logo"/>
+                        <img src={Logo} alt="logo" />
                     </Link>
                 </div>
                 <div className="menu">
-                    <ul className='flex-wrap'>
+                    <ul className="flex-wrap br-m">
                         <li className={location.pathname === "/" ? "on" : ""}>
                             <Link to="/">
-                                {/* <FontAwesomeIcon icon={faHouse} /> */}
-                                홈
+                                {/* <FontAwesomeIcon icon={faHouse} /> */}홈
                             </Link>
                         </li>
-                        <li className={location.pathname === "/social" ? "on" : ""}>
+                        <li
+                            className={
+                                location.pathname.split("/")[1] === "social" ? "on" : ""
+                            }
+                        >
                             <Link to="/social">
                                 {/* <FontAwesomeIcon icon={faHouse} /> */}
                                 소셜
                             </Link>
                         </li>
-                        <li className={location.pathname === "/blog" ? "on" : ""}>
+                        <li
+                            className={
+                                location.pathname.split("/")[1] === "blog" ? "on" : ""
+                            }
+                        >
                             <Link to="/blog">
                                 {/* <FontAwesomeIcon icon={faHouse} /> */}
                                 블로그
@@ -40,8 +49,15 @@ const Header = () => {
                         </li>
                     </ul>
                 </div>
-                <div className='info'>
-                    각종
+                <div className="func flex-wrap">
+                    <button type="button" className="setting-btn">
+                        <FontAwesomeIcon icon={faGear}></FontAwesomeIcon>
+                    </button>
+                    <div className='search-wrap'>
+                        <div className='search-btn'>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
